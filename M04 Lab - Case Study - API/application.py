30453845f62b64216e6b7cbe2d20eb9bc898d545
Donner-von-Swabia <122878842+Donner-von-Swabia@users.noreply.github.com>
2023-02-08 11:13:00 -0500

    M04

---
+++ M04 Lab - Case Study - API/application.py
@@ -0,0 +1,34 @@
+from flask import Flask
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(250), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    publisher = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f"{self.id} - {self.book_name} - {self.author} - {self.publisher}"
+
+@app.route('/')
+def hello_world():
+    return "hello world"
+
+@app.route('/books')
+def get_books():
+    book = books.query.all()
+    output = []
+    for books in book:
+        books_data = {'id':book.id,'book_name':book.book_name,'author':book.author,'publisher':book.publisher}
+    output.append(books_data)
+    return output
+
+@app.route('/books/<id>')
+def get_books():
+    book = books.query.get_or_404(id)
+    return {'id':book.id,'book_name':book.book_name,'author':book.author,'publisher':book.publisher}
